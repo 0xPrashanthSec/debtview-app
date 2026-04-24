ba0b7a06f423f701015066f9d5d7ebdf6615f3e0
0xPrashanthSec <40313110+0xPrashanthSec@users.noreply.github.com>
2026-04-24 23:35:29 +0530

    Change base path in Vite config to '/debtview-app/'

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/loan-tracker/', // must match your GitHub repo name
+  base: '/debtview-app/', // must match your GitHub repo name
 })
